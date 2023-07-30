b41d2bd2b087f032c89bb0eff70841fac2cbc66e
agrim <agrim4sure@gmail.com>
2023-07-30 20:15:02 +0530

    code of lesson 1

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag")
+  )
+);
+console.log(parent); //object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
